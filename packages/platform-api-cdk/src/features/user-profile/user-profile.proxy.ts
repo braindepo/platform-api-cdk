761aaf6a9e9ef490c3cd5701a2a3efc726bde7d0
ValeryYuntsevich <Valery.Yuntsevich@juriba.com>
2023-03-02 14:59:43 +0300

    Add some methods in the user-profile proxy

--- packages/platform-api-cdk/src/features/user-profile/user-profile.proxy.ts
+++ packages/platform-api-cdk/src/features/user-profile/user-profile.proxy.ts
@@ -2,6 +2,7 @@ import { Axios } from 'axios';
 
 import { IUser } from '../../shared/users';
 import { PlatformProxy } from '../../shared/proxies';
+import { IUpdateUserProfile, IUpdateUserSecurity } from './models';
 
 export class UserProfileProxy extends PlatformProxy {
   private readonly baseProxyUrl = '/user-profile';
@@ -12,5 +13,17 @@ export class UserProfileProxy extends PlatformProxy {
 
   async findOne(): Promise<IUser> {
     return this.axiosInstance.get(this.baseProxyUrl).then((response) => JSON.parse(response.data));
+  }
+
+  async updateSecurity(userSecurity: IUpdateUserSecurity): Promise<IUser> {
+    return this.axiosInstance
+      .put(`${this.baseProxyUrl}/security`, userSecurity)
+      .then((response) => JSON.parse(response.data));
+  }
+
+  async updateProfile(userProfile: IUpdateUserProfile): Promise<IUser> {
+    return this.axiosInstance
+      .put(`${this.baseProxyUrl}/profile`, userProfile)
+      .then((response) => JSON.parse(response.data));
   }
 }
