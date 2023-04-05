eb251b5e9333113592af32e1403defa5f121bb9b
Mankov Dima <dima.mankov@mail.ru>
2023-04-05 22:16:24 +0300

    [Features:Auth] Base post method was added to platform-proxy class. Response handler was added to  platform-proxy class.

--- packages/platform-api-cdk/src/features/auth/auth.proxy.ts
+++ packages/platform-api-cdk/src/features/auth/auth.proxy.ts
@@ -14,45 +14,31 @@ import {
 } from './models';
 
 export class AuthProxy extends PlatformProxy {
-  private readonly baseProxyUrl = '/auth';
-
   constructor(axiosInstance: Axios) {
-    super(axiosInstance);
+    super(axiosInstance, '/auth');
   }
 
   async login(loginPassword: ILoginPasswordAuthData): Promise<IUserAuthConfig> {
-    return this.axiosInstance
-      .post(`${this.baseProxyUrl}/login`, JSON.stringify(loginPassword))
-      .then((response) => JSON.parse(response.data));
+    return this.post('/login', loginPassword);
   }
 
   async register(register: IRegisterData): Promise<number> {
-    return this.axiosInstance
-      .post(`${this.baseProxyUrl}/register`, JSON.stringify(register))
-      .then((response) => JSON.parse(response.data));
+    return this.post('/register', register);
   }
 
-  async updateEmailVerificationToken(data: INewEmailVerificationTokenData): Promise<void> {
-    return this.axiosInstance
-      .post(`${this.baseProxyUrl}/update-email-verification-token`, JSON.stringify(data))
-      .then((response) => JSON.parse(response.data));
+  async renewEmailVerificationToken(data: INewEmailVerificationTokenData): Promise<void> {
+    return this.post('/renew-email-verification-token', data);
   }
 
   async verifyEmail(data: IEmailVerificationData): Promise<IEmailVerificationResult> {
-    return this.axiosInstance
-      .post(`${this.baseProxyUrl}/verify-email`, JSON.stringify(data))
-      .then((response) => JSON.parse(response.data));
+    return this.post('/verify-email', data);
   }
 
   async verifyJwt(authTokenData: ITokenAuthData): Promise<IAuthToken> {
-    return this.axiosInstance
-      .post(`${this.baseProxyUrl}/verify-jwt`, JSON.stringify(authTokenData))
-      .then((response) => JSON.parse(response.data));
+    return this.post('/verify-jwt', authTokenData);
   }
 
   async verifyApiToken(authTokenData: ITokenAuthData): Promise<IAuthToken> {
-    return this.axiosInstance
-      .post(`${this.baseProxyUrl}/verify-api-token`, JSON.stringify(authTokenData))
-      .then((response) => JSON.parse(response.data));
+    return this.post('/verify-api-token', authTokenData);
   }
 }
--- packages/platform-api-cdk/src/shared/proxies/platform.proxy.ts
+++ packages/platform-api-cdk/src/shared/proxies/platform.proxy.ts
@@ -1,5 +1,22 @@
-import { Axios } from 'axios';
+import { Axios, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export class PlatformProxy {
-  constructor(protected readonly axiosInstance: Axios) {}
+  constructor(protected readonly axiosInstance: Axios, protected readonly baseUrl?: string) {}
+
+  protected post<T = unknown, R = AxiosResponse<T>, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig<D>,
+  ): Promise<R> {
+    return this.axiosInstance
+      .post(`${this.baseUrl}${url}`, JSON.stringify(data), config as AxiosRequestConfig<string>)
+      .then(this.handleResponse<R>);
+  }
+
+  protected handleResponse = <T = unknown>(response: AxiosResponse<string>): T => {
+    if (response.status >= 300) {
+      throw response;
+    }
+    return response.data ? JSON.parse(response.data) : response.data;
+  };
 }
